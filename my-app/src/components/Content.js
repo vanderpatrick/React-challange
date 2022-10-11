04a063802576f4f7f29dbc874590cca714b3a82a
vanderpatrick <patrick123321123@icloud.com>
2022-10-11 13:24:26 +0000

    more stuff

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import PostItem from './Postitem'
 import { savedPosts } from "../posts.json"
+import Loader from './Loader'
 
 
 export class Content extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLoaded: false,
+    }
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      })
+    },3000) 
+  }
+
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}><h1>My Photos</h1>
         </div>
-        <div className={css.SearchResults}>
+        <div className={css.SearchResults}>{this.state.isLoaded ? (
           <PostItem savedPosts = {savedPosts} />
+          ) : (<Loader />)
+      }
         </div>
       </div>
     )

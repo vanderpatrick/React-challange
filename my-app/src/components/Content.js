0fcb7caa839b7b790a1c3da5e0ed8de14e7430ee
vanderpatrick <patrick123321123@icloud.com>
2022-10-11 16:13:52 +0000

    add stuff

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -11,23 +11,45 @@ export class Content extends Component {
   
     this.state = {
        isLoaded: false,
+       posts: [],
     }
   }
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         isLoaded: true,
+        posts: savedPosts,
       })
     },3000) 
   }
 
+  handleChange = (event) => {
+    const name = event.target.value.toLowerCase()
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name);
+    })
+    this.setState({
+      posts: filteredPosts
+    })
+  }
+  
   render() {
     return (
       <div className={css.Content}>
-        <div className={css.TitleBar}><h1>My Photos</h1>
+        <div className={css.TitleBar}>
+          <h1>My Photos</h1>
+          <form>
+            <label htmlFor='searchInput'>Search: {this.state.posts.length} </label>
+            <input
+            type="search"
+            id="searchInput"
+            onChange={(event) => {this.handleChange(event)}}
+            />
+            <h4>Post Found: </h4>
+          </form>
         </div>
         <div className={css.SearchResults}>{this.state.isLoaded ? (
-          <PostItem savedPosts = {savedPosts} />
+          <PostItem savedPosts={this.state.posts} />
           ) : (<Loader />)
       }
         </div>
